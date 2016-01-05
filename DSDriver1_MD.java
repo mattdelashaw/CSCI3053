9948d89bc62227807ca1a4171c0b4762c952d117
Matt Delashaw <mattdelashaw@gmail.com>
2016-01-04 20:32:08 -0600

    Create DSDriver1_MD.java

---
+++ DSDriver1_MD.java
@@ -0,0 +1,32 @@
+/**
+ * @(#)DSDriver1_MD.java
+ *
+ *
+ * @author matt delashaw
+ * @version 1.00 2014/9/17
+ */
+import javax.swing.JOptionPane;
+
+public class DSDriver1_MD {
+
+    public static void main(String[] args){
+
+
+		String name;
+    	DataStructure1 d = new DataStructure1();
+    	StudentListings s = new StudentListings();
+    	StudentListings u = new StudentListings();
+
+		for(int i=0; i <3; i++){
+    		s.input();
+    		d.insert(s);
+    	}
+		System.out.println(d.fetch(name = JOptionPane.showInputDialog("search name")));
+		System.out.println(d.delete(name = JOptionPane.showInputDialog("Student to delete")));
+		u.input();
+		System.out.println(d.update(name = JOptionPane.showInputDialog("Student to update"), u));
+    	d.showAll();
+    }
+
+
+}
